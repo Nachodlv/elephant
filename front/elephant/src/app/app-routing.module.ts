cda217c825dfb2effa5f9df417206503630593f0
Numa Leone <numaleoneelizalde@gmail.com>
2020-08-24 21:31:08 -0300

    algo

--- front/elephant/src/app/app-routing.module.ts
+++ front/elephant/src/app/app-routing.module.ts
@@ -1,23 +1,21 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import {ExampleGuard} from './guards/example.guard';
 import {HomeComponent} from './components/home/home.component';
 import {NoteCreatorComponent} from "./components/note-creator/note-creator.component";
 import {NoteComponent} from "./components/note/note.component";
+import {RegisterComponent} from "./components/register/register.component";
 
 
 const routes: Routes = [
-  {
-    path: 'home', component: HomeComponent
-    //, canActivate: [ExampleGuard]
-  },
   {path: 'note/create', component: NoteCreatorComponent},
-  {path: 'note', component: NoteComponent}
+  {path: 'note', component: NoteComponent},
+  { path: 'register', component: RegisterComponent },
+  { path: 'home', component: HomeComponent, canActivate: [ExampleGuard] }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule { }
--- front/elephant/src/app/app.module.ts
+++ front/elephant/src/app/app.module.ts
@@ -2,25 +2,18 @@ import {BrowserModule} from '@angular/platform-browser';
 import {ErrorHandler, NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppMaterialModule} from './app-material.module';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatCardModule} from '@angular/material/card';
 import {ErrorService} from './services/error.service';
 import {ExamplePipe} from './pipes/time-left.pipe';
-import { HomeComponent } from './components/home/home.component';
+import {HomeComponent} from './components/home/home.component';
 import { NoteComponent } from './components/note/note.component';
 import { NoteCreatorComponent } from './components/note-creator/note-creator.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './components/register/register.component';
+import {EqualValidator} from './directives/equal-validator.directive';
 
 @NgModule({
   declarations: [
@@ -29,18 +22,21 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     HomeComponent,
     NoteComponent,
     NoteCreatorComponent,
-    NavbarComponent
+    NavbarComponent,
+    RegisterComponent,
+    EqualValidator
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    AppMaterialModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule
   ],
   providers: [
-    { provide: ErrorHandler, useClass: ErrorService }
+    {provide: ErrorHandler, useClass: ErrorService}
   ],
   bootstrap: [AppComponent]
 })
