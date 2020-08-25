dc47df67911aedb6f9295f3e8d664af6679d0006
Numa Leone <numaleoneelizalde@gmail.com>
2020-08-25 18:14:01 -0300

    boton de crear nota terminado

--- front/elephant/src/app/models/Note.ts
+++ front/elephant/src/app/models/Note.ts
@@ -1,14 +1,16 @@
-import {Timestamp} from "rxjs";
-
 export class Note {
   uuid: number;
   title: string;
   content: string;
   created: string;
 
-  constructor(id: number, name: string) {
+  constructor(id?: number, name?: string) {
     this.uuid = id;
     this.title = name;
+  }
+
+  static fromJson(data: any): Note {
+    return Object.assign(new Note(), data);
   }
 
 }
