f00c265d9ae61e65f5bbcf10c7cf78641e34c87d
pedro <paraoz22@gmail.com>
2020-08-29 22:54:06 -0300

    Catched the TokenExpiredException

--- back/src/main/java/com/lab/elephant/security/JWTAuthorizationFilter.java
+++ back/src/main/java/com/lab/elephant/security/JWTAuthorizationFilter.java
@@ -2,6 +2,7 @@ package com.lab.elephant.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -37,23 +38,27 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
       return;
     }
     
-    UsernamePasswordAuthenticationToken authentication = getAuthentication(req);
+    UsernamePasswordAuthenticationToken authentication = getAuthentication(req, res);
     
     SecurityContextHolder.getContext().setAuthentication(authentication);
     chain.doFilter(req, res);
   }
   
-  private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+  private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String token = request.getHeader(HEADER_STRING);
     if (token != null) {
       // parse the token.
-      String user = JWT.require(Algorithm.HMAC512(SECRET.getBytes()))
-              .build()
-              .verify(token.replace(TOKEN_PREFIX, ""))
-              .getSubject();
-      
-      if (user != null) {
-        return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+      String user;
+      try {
+        user = JWT.require(Algorithm.HMAC512(SECRET.getBytes()))
+                .build()
+                .verify(token.replace(TOKEN_PREFIX, ""))
+                .getSubject();
+        if (user != null) {
+          return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+        }
+      } catch (TokenExpiredException e) {
+        //todo ask about this! :)
       }
       return null;
     }
