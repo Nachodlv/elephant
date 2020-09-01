4b2dc12f5287de7ef021649325c467a5087321d8
BraianBiale <braianbiale@gmail.com>
2020-09-01 17:36:49 -0300

    Change routing of login to "/";

--- front/elephant/src/app/guards/auth.guard.ts
+++ front/elephant/src/app/guards/auth.guard.ts
@@ -20,7 +20,7 @@ export class AuthGuard implements CanActivate {
     if (this.authService.authenticate()) {
       return true;
     }
-    this.router.navigate(['/login']);
+    this.router.navigate(['/']);
     return false;
   }
 
