42e53126ad59d2eeba0fdad52ca59032e27d8ca6
Numa Leone <numaleoneelizalde@gmail.com>
2020-09-04 17:53:28 -0300

    changed style of navbar and made small changes higlighted by Gianluca Scolaro

--- front/elephant/src/app/components/note/note.component.ts
+++ front/elephant/src/app/components/note/note.component.ts
@@ -15,48 +15,12 @@ export class NoteComponent implements OnInit {
   }
 
   openDialog(): void {
-   this.dialog.open(NoteCreatorComponent, {
-        width: '400px',
-        height: '230px',
-      });
-  }
-
-  ngOnInit(): void {
-  }
-
-  /*
-  disabled(): boolean {
-    return this.noteForm.value.title === '';
-  }
-  */
-}
-
-
-/*
-export class NoteComponent implements OnInit {
-  noteForm: FormGroup;
-  submitted = false;
-
-  constructor(private formBuilder: FormBuilder) {
-  }
-
-  ngOnInit(): void {
-    this.noteForm = this.formBuilder.group({
-      title: ['', Validators.required]
+    this.dialog.open(NoteCreatorComponent, {
+      width: '400px',
+      height: '230px',
     });
-
-  }
-  disabled(): boolean {
-    return this.noteForm.value.title === '';
   }
 
-  createNote(): void {
-    console.log(this.noteForm.controls.title);
-    note: Note = new Note(id,title);
-    la logica de cuando se hace una nota
-    send to Back/ api
-
+  ngOnInit(): void {
   }
 }
-*/
-
