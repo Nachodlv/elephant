2d0bd26753361b118dca32ae4e21b079c9c8f478
maxiadaro <Sirius2020>
2020-09-10 15:55:20 -0300

    Added JsonProperty write only to user password Changed ObjectMapper from user controller tests

--- back/src/main/java/com/lab/elephant/model/User.java
+++ back/src/main/java/com/lab/elephant/model/User.java
@@ -1,6 +1,7 @@
 package com.lab.elephant.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -12,66 +13,68 @@ public class User {
   @Id
   @GeneratedValue
   private long uuid;
-  
+
   @NotNull
   private String lastName;
   @NotNull
   private String firstName;
   @NotNull
   private String email;
+
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
   @NotNull
   private String password;
-  
+
   @JsonIgnore
   @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
   private List<Comment> comments = new ArrayList<>();
 
   public User() {
   }
-  
+
   public User(String firstName, String lastName, String email, String password) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
     this.password = password;
   }
-  
+
   public long getUuid() {
     return uuid;
   }
-  
+
   public String getFirstName() {
     return firstName;
   }
-  
+
   public void setFirstName(String firstName) {
     this.firstName = firstName;
   }
-  
+
   public String getLastName() {
     return lastName;
   }
-  
+
   public void setLastName(String lastName) {
     this.lastName = lastName;
   }
-  
+
   public String getEmail() {
     return email;
   }
-  
+
   public void setEmail(String email) {
     this.email = email;
   }
-  
+
   public String getPassword() {
     return password;
   }
-  
+
   public void setPassword(String password) {
     this.password = password;
   }
-  
+
   public void setUuid(long uuid) {
     this.uuid = uuid;
   }
