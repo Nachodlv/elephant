bb177b02303f4f746d1f2dfbb6414b87861e0342
Numa Leone <numaleoneelizalde@gmail.com>
2020-09-15 09:56:39 -0300

    `small changes to see profile html`

--- back/src/main/java/com/lab/elephant/security/SecurityConstants.java
+++ back/src/main/java/com/lab/elephant/security/SecurityConstants.java
@@ -7,4 +7,5 @@ public class SecurityConstants {
   public static final String HEADER_STRING = "Authorization";
   public static final String SIGN_UP_URL = "/user/create";
   public static final String TOKEN_VERIFY_URL = "/token/verify";
+  public static final String LOGOUT_URL = "/logout";
 }
