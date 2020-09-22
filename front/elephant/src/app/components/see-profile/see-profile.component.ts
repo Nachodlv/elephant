32b088bdd332c95862dd630a64317b8d00971a32
BraianBiale <braianbiale@gmail.com>
2020-09-22 20:08:04 -0300

    added update password dialog; fix some bugs at see profile;

--- front/elephant/src/app/components/see-profile/see-profile.component.ts
+++ front/elephant/src/app/components/see-profile/see-profile.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from '../../services/user.service';
 import {User} from '../../models/user-model';
+import {UpdatePasswordDialogComponent} from '../update-password-dialog/update-password-dialog.component';
+import {MatDialog} from '@angular/material/dialog';
 
 
 @Component({
@@ -12,13 +14,18 @@ export class SeeProfileComponent implements OnInit {
 
   user: User;
 
-  constructor(private userService: UserService) {
+  loading = true;
+
+  constructor(
+    private userService: UserService,
+    private dialog: MatDialog
+  ) {
   }
 
   ngOnInit(): void {
     this.userService.getUser().subscribe(res => {
       this.user = res;
-      console.log(this.user);
+      this.loading = false;
     });
 
   }
@@ -26,4 +33,10 @@ export class SeeProfileComponent implements OnInit {
   saveProfile(): void {
   }
 
+  openUpdatePasswordDialog(): void {
+    this.dialog.open(UpdatePasswordDialogComponent, {
+      width: '30vw',
+      position: {top: '10%'}
+    });
+  }
 }
