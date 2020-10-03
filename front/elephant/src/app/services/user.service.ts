f70bc0680fb03d02e5f54c448a40b568850895cd
BraianBiale <braianbiale@gmail.com>
2020-10-02 21:09:51 -0300

    remove unused imports;

--- front/elephant/src/app/services/user.service.ts
+++ front/elephant/src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Observable, of} from 'rxjs';
+import {Observable} from 'rxjs';
 import {HttpService} from './http.service';
 import {User} from '../models/user-model';
 import {map, tap} from 'rxjs/operators';
