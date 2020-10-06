db522b648bd6f7944f9d52deaebf595d91f861cb
BraianBiale <braianbiale@gmail.com>
2020-10-06 19:11:14 -0300

    added see all notes implementation; change some styles at note component;

--- front/elephant/src/app/components/note/note.component.ts
+++ front/elephant/src/app/components/note/note.component.ts
@@ -1,30 +1,53 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import {NoteCreatorComponent} from '../note-creator/note-creator.component';
 import {Note} from '../../models/note-model';
 import {NoteService} from '../../services/note.service';
+import {Router} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-note',
   templateUrl: './note.component.html',
   styleUrls: ['./note.component.scss']
 })
-export class NoteComponent implements OnInit {
+export class NoteComponent implements OnInit, OnDestroy {
 
   notes: Note[] = [];
   loaded = false;
   filterString = '';
 
+  notesSubscription: Subscription;
+
   constructor(private dialog: MatDialog,
               private noteService: NoteService,
+              private router: Router
   ) {
   }
 
   ngOnInit(): void {
-    this.noteService.getAllNotes().subscribe(res => {
-      this.notes = res;
+    this.loadNotes();
+  }
+
+  ngOnDestroy(): void {
+    this.notesSubscription?.unsubscribe();
+  }
+
+  loadNotes(): void {
+    this.notesSubscription = this.noteService.getAllNotes().subscribe(res => {
+      this.notes = this.resolveNotesData(res);
       this.loaded = true;
     });
+  }
+
+  resolveNotesData(notes): Note[] {
+    return notes.map(note => {
+      return {...note, content: this.truncateContent(note.content)};
+    });
+  }
+
+  truncateContent(content): string {
+    return content.substring(0, 300).concat('...');
   }
 
   openDialog(): void {
@@ -36,5 +59,6 @@ export class NoteComponent implements OnInit {
 
   openNote(note): void {
     console.log(note);
+    this.router.navigate(['/note/', note.uuid]);
   }
 }
