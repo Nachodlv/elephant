a962a38eabd06c9650853b633cf080879cdd4eee
MaxiAdaro <maxiadaro1999@gmail.com>
2020-10-13 21:22:54 -0300

    redirect authenticate user from login or register page to home

---
+++ front/elephant/src/app/guards/login-auth.guard.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthService} from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginAuthGuard implements CanActivate {
+
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {
+  }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    if (this.authService.authenticate()) {
+      this.router.navigate(['/home']);
+      return false;
+    }
+    return true;
+  }
+
+}
