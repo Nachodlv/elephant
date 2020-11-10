faa9a2c44bd1cc75faa7f0dd5fc3f56f698e00ef
Numa Leone <numaleoneelizalde@gmail.com>
2020-11-10 01:36:59 -0300

    started the feature and made the links work

--- front/elephant/src/app/app.module.ts
+++ front/elephant/src/app/app.module.ts
@@ -4,34 +4,34 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppMaterialModule} from './app-material.module';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {ErrorService} from './services/error.service';
 import {ExamplePipe} from './pipes/time-left.pipe';
 import {HomeComponent} from './components/home/home.component';
-import { NoteComponent } from './components/note/note.component';
-import { NoteCreatorComponent } from './components/note-creator/note-creator.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { RegisterComponent } from './components/register/register.component';
+import {NoteComponent} from './components/note/note.component';
+import {NoteCreatorComponent} from './components/note-creator/note-creator.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {RegisterComponent} from './components/register/register.component';
 import {EqualValidator} from './directives/equal-validator.directive';
-import { MatDialogModule} from '@angular/material/dialog';
+import {MatDialogModule} from '@angular/material/dialog';
 import {LoginComponent} from './components/login/login.component';
-import { SeeNoteComponent } from './components/see-note/see-note.component';
-import { ShareNoteDialogComponent } from './components/share-note-dialog/share-note-dialog.component';
-import { CommentComponent } from './components/comment/comment.component';
+import {SeeNoteComponent} from './components/see-note/see-note.component';
+import {ShareNoteDialogComponent} from './components/share-note-dialog/share-note-dialog.component';
+import {CommentComponent} from './components/comment/comment.component';
 import {SeeProfileComponent} from './components/see-profile/see-profile.component';
-import { UpdatePasswordDialogComponent } from './components/update-password-dialog/update-password-dialog.component';
-import { TagNoteComponent } from './components/tag-note/tag-note.component';
-import { FilterNotePipe } from './pipes/filter-note.pipe';
-import { DeleteNoteDialogComponent } from './components/delete-note-dialog/delete-note-dialog.component';
-import { EditNoteComponent } from './components/edit-note/edit-note.component';
-import { PrintNoteComponent } from './components/print-note/print-note.component';
-import {MarkdownModule} from 'ngx-markdown';
-import { DeleteUserDialogComponent } from './components/delete-user-dialog/delete-user-dialog.component';
+import {UpdatePasswordDialogComponent} from './components/update-password-dialog/update-password-dialog.component';
+import {TagNoteComponent} from './components/tag-note/tag-note.component';
+import {FilterNotePipe} from './pipes/filter-note.pipe';
+import {DeleteNoteDialogComponent} from './components/delete-note-dialog/delete-note-dialog.component';
+import {EditNoteComponent} from './components/edit-note/edit-note.component';
+import {PrintNoteComponent} from './components/print-note/print-note.component';
+import {MarkdownModule, MarkedOptions, MarkedRenderer} from 'ngx-markdown';
+import {DeleteUserDialogComponent} from './components/delete-user-dialog/delete-user-dialog.component';
 import 'prismjs/prism';
 import 'prismjs/components/prism-typescript.min.js';
 import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
-import { EditNotePermissionsDialogComponent } from './components/edit-note-permissions-dialog/edit-note-permissions-dialog.component';
+import {EditNotePermissionsDialogComponent} from './components/edit-note-permissions-dialog/edit-note-permissions-dialog.component';
 
 @NgModule({
   declarations: [
@@ -66,7 +66,15 @@ import { EditNotePermissionsDialogComponent } from './components/edit-note-permi
     FormsModule,
     ReactiveFormsModule,
     MatDialogModule,
-    MarkdownModule.forRoot(),
+    MarkdownModule.forRoot(
+      {
+        loader: HttpClient,
+        markedOptions: {
+          provide: MarkedOptions,
+          useFactory: markedOptionsFactory,
+        },
+      }
+    ),
   ],
   providers: [
     {provide: ErrorHandler, useClass: ErrorService}
@@ -75,3 +83,48 @@ import { EditNotePermissionsDialogComponent } from './components/edit-note-permi
 })
 export class AppModule {
 }
+
+export function markedOptionsFactory(): MarkedOptions {
+  const renderer = new MarkedRenderer();
+
+  renderer.blockquote = (text: string) => {
+    return '<blockquote class="blockquote"><p>' + text + '</p></blockquote>';
+  };
+
+  // renderer.listitem = text => {
+  //   if (text.startsWith('[ ]') === true) {
+  //     text = text.replace('[ ]', '');
+  //     return `<li class="list-checkbox-item"><i class="fal fa-fw falistitem fa-square"></i>${text}</li>`;
+  //   }
+  //   if (text.startsWith('[x]') === true) {
+  //     text = text.replace('[x]', '');
+  //     return `<li class="list-checkbox-item"><i class="fal fa-fw falistitem fa-check-square"></i>${text}</li>`;
+  //   }
+  //   if (text.startsWith('[-]') === true) {
+  //     text = text.replace('[-]', '');
+  //     return `<li class="list-checkbox-item"><i class="fal fa-fw falistitem fa-minus-square"></i>${text}</li>`;
+  //   }
+  //   return `<li>${text}</li>`;
+  // };
+  renderer.listitem = (text) => {
+    if (/^\s*\[[x ]\]\s*/.test(text)) {
+      text = text
+        .replace(/^\s*\[ \]\s*/, '<i class="empty checkbox icon"></i> ')
+        .replace(/^\s*\[x\]\s*/, '<i class="checked checkbox icon"></i> ');
+      return '<li style="list-style: none">' + text + '</li>';
+    } else {
+      return '<li>' + text + '</li>';
+    }
+  };
+
+  return {
+    renderer,
+    gfm: true,
+    breaks: false,
+    pedantic: false,
+    smartLists: true,
+    smartypants: false,
+  };
+}
+
+
